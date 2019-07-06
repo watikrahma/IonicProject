9a57ce389ae0bb2b2d8afb020b8dbd8355da584d
IT R1 <its.rahmawati@gmail.com>
2019-07-06 11:28:28 +0700

    first commit

---
+++ src/app/pages/menu/menu.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MenuPage } from './menu.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuPage,
+    children: [ //membaca content yang akan ada di menu
+      { path: 'first-with-tabs', loadChildren: '../first-with-tabs/first-with-tabs.module#FirstWithTabsPageModule'},
+      //path: <nama page>, loadChildren: <direktori>
+      { path: 'second', loadChildren: '../second/second.module#SecondPageModule'},
+      { path: 'firebasepage', loadChildren: '../firebasepage/firebasepage.module#FirebasepagePageModule'}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [MenuPage]
+})
+export class MenuPageModule {}
