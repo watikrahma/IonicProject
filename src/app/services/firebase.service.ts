4338dade6012e29d68adfea61026314bbeeaa2ac
IT R1 <its.rahmawati@gmail.com>
2019-07-13 11:42:14 +0700

    add firebase service & auth

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -3,8 +3,8 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { from } from 'rxjs';
 import { resolve } from 'path';
+import { from } from 'rxjs';
 import { reject } from 'q';
 
 @Injectable({
@@ -47,7 +47,82 @@ export class FirebaseService {
   }
 
   // unsubscribe perintah
-  unsubscribeOnLogOut(){
+  unsubscribeOnLogOut() {
     this.snapshotChangesSubcription.unsubscribe();
+  }
+
+  //membuat task baru
+  createTask(value) {
+    return new Promise<any>((resolve, reject) => {
+      let currentUser = firebase.auth().currentUser;
+      this.afs.collection('people').doc(currentUser.uid)
+        .collection('tasks').add({
+          title: value.title,
+          description: value.description,
+          image: value.image
+        })
+        .then(
+          res => resolve(res),
+          err => reject(err)
+        )
+    })
+  }
+
+  //untuk menghandle image
+  encodeImageUri(imageUri, callback) {
+    var c = document.createElement('canvas');
+    var ctx = c.getContext("2d");
+    var img = new Image();
+
+    img.onload = function () {
+      var aux: any = this;
+      c.width = aux.width;
+      c.height = aux.height;
+      ctx.drawImage(img, 0, 0);
+      var dataURL = c.toDataURL("/image/jpeg");
+      callback(dataURL);
+    }
+  }
+
+  //meng upload image ke firebase
+  uploadImage(imageUri, randomID) {
+    return new Promise<any>((resolve, reject) => {
+      let storageRef = firebase.storage().ref();
+      let imageRef = storageRef.child('image')
+        .child(randomID);
+      this.encodeImageUri(imageUri, function (image64) {
+        imageRef.putString(image64, 'data_url')
+          .then(snapshot => {
+            snapshot.ref.getDownloadURL()
+              .then(res => resolve(res))
+          }, err => {
+            reject(err);
+          })
+      })
+    })
+  }
+  //mengubah task
+  updateTask(taskKey, value) {
+    return new Promise<any>((resolve, reject) => {
+      let currentUser = firebase.auth().currentUser;
+      this.afs.collection('people')
+        .doc(currentUser.uid)
+        .collection('task')
+        .doc(taskKey).set(value)
+        .then(res => resolve(res),
+          err => reject(err))
+    })
+  }
+  //menghapus task
+  deleteTask(taskKey) {
+    return new Promise<any>((resolve, reject) => {
+      let currentUser = firebase.auth().currentUser;
+      this.afs.collection('people')
+        .doc(currentUser.uid)
+        .collection('task')
+        .doc(taskKey).delete()
+        .then(res => resolve(res),
+          err => reject(err))
+    })
   }
 }
